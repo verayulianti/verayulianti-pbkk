e7902078179d6d0c0f3fd58661422563a9338280
verayulianti <yltvera02122002@gmail.com>
2023-05-18 10:43:51 +0700

    first commit

---
+++ app/Http/Controllers/konsumen/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\konsumen;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/kurir/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\kurir;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+}
